9fdc401985401f7a66b592eb451a657134fdee2e
Andile <112043128+ANDI-softwaredev@users.noreply.github.com>
2024-05-16 05:52:29 -0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is the PRIMARY purpose of the Ministry of Skills Audit and Development?",
+    answer: "To conduct skills audits and address skill gaps",
+    options: [
+      "To provide career counselling services",
+      "To conduct skills audits and address skill gaps",
+      "To manage the recruitments for private firms",
+      "To manage government communication"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which one of the following is NOT one of the Ministry’s Core Values?",
+    answer: "Return on Investment",
+    options: [
+      "Gender Justice",
+      "Consistency",
+      "Inclusivity",
+      "Return on Investment"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is Vision 2030 for Zimbabwe?",
+    answer: "To achieve an upper-middle-income economy",
+    options: [
+      "To become a leading exporter in agriculture",
+      "To achieve an upper-middle-income economy",
+      "To increase the number of universities",
+      "To have more enterpreneurs"
+    ]
+  },
+  {
+    numb: 4,
+    question: "How often does the Ministry conduct a National Critical Skills Audit?",
+    answer: "Every 10 years",
+    options: [
+      "Every 3 years",
+      "Every 10 years",
+      "Every 5 years ",
+      "Every 15 years"
+    ]
+  },
+{
+    numb: 5,
+    question: "Who is the Minister of the Ministry of Skills Audit & Development?",
+    answer: "Hon Paul Mavima",
+    options: [
+      "Hon Paul Mavima",
+      "Hon Peter Madzima",
+      "Hon Paul Dzingai",
+      "Hon Peter Dzingai"
+    ]
+  },
+];
